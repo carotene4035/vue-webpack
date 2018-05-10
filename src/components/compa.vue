1edbac75390024f5532fdfa2ef5314cd597c8d4b
carotene512 <karoten512@gmail.com>
2018-05-11 00:02:54 +0900

    routing 実験

---
+++ src/components/compa.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>aaaaaa</div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Compa from '@/components/compa'
+import Compb from '@/components/compb'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/a',
+      name: 'compa',
+      component: Compa
+    },
+    {
+      path: '/b',
+      name: 'compb',
+      component: Compb
     }
   ]
 })
